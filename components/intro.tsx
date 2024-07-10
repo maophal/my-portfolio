a153dd5e16c08957e13425bb3c1a7779da2ca50b
Tayes <tayes@Phals-Mac-mini.local>
2024-07-10 13:50:17 +0700

    update: image

--- components/intro.tsx
+++ components/intro.tsx
@@ -10,7 +10,7 @@ import { FaGithubSquare } from "react-icons/fa";
 import { useSectionInView } from "@/lib/hooks";
 import { useActiveSectionContext } from "@/context/active-section-context";
 import { BsTelegram } from "react-icons/bs";
-
+import * as meImage from "../public/me.jpg"
 export default function Intro() {
   const { ref } = useSectionInView("Home", 0.5);
   const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
@@ -32,7 +32,7 @@ export default function Intro() {
             }}
           >
             <Image
-              src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?fit=crop&w=368&h=368&q=100"
+              src={meImage}
               alt="Phal portrait"
               width="192"
               height="192"
