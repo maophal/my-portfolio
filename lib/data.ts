a98b5380fe36c4551cc4dcf3a0dbfa5237d231d5
Tayes <tayes@Phals-Mac-mini.local>
2024-07-10 14:32:26 +0700

    update: data

--- lib/data.ts
+++ lib/data.ts
@@ -1,11 +1,8 @@
 import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
-import { FaReact } from "react-icons/fa";
-import { LuGraduationCap } from "react-icons/lu";
-import corpcommentImg from "@/public/corpcomment.png";
-import rmtdevImg from "@/public/rmtdev.png";
-import wordanalyticsImg from "@/public/wordanalytics.png";
-
+import SkyAngkor from "@/public/my-project/skyangkor.png"
+import AAndP from "@/public/my-project/a-and-p.png";
+import DGT   from "@/public/my-project/dgt.png"
 export const links = [
   {
     name: "Home",
@@ -35,12 +32,20 @@ export const links = [
 
 export const experiencesData = [
   {
-    title: "Graduated bootcamp",
-    location: "Miami, FL",
+    title: "Junior Web Developer",
+    location: "Mylekha Co.,LTD",
     description:
-      "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
-    icon: React.createElement(LuGraduationCap),
+      "i working verify hard at Mylekha. and learn a lot from that warming work place.",
+    icon: React.createElement(CgWorkAlt),
     date: "2018",
+  },
+  {
+    title: "Graduated Computer Scients",
+    location: "CUS, Phnom Penh",
+    description:
+      "I graduated after 4 years of studying. I immediately found a job as a junior developer.",
+    icon: React.createElement(CgWorkAlt),
+    date: "2020",
   },
   {
     title: "Front-End Developer",
@@ -55,32 +60,32 @@ export const experiencesData = [
     location: "Houston, TX",
     description:
       "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
-    icon: React.createElement(FaReact),
+    icon: React.createElement(CgWorkAlt),
     date: "2021 - present",
   },
 ] as const;
 
 export const projectsData = [
   {
-    title: "CorpComment",
+    title: "Sky Angkor Air",
     description:
       "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
     tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
-    imageUrl: corpcommentImg,
+    imageUrl: SkyAngkor,
   },
   {
-    title: "rmtDev",
+    title: "A&P Accounting",
     description:
       "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
     tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
-    imageUrl: rmtdevImg,
+    imageUrl: AAndP,
   },
   {
-    title: "Word Analytics",
+    title: "គ្រូបង្រៀនឌីជីថល",
     description:
       "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
     tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-    imageUrl: wordanalyticsImg,
+    imageUrl: DGT,
   },
 ] as const;
 
